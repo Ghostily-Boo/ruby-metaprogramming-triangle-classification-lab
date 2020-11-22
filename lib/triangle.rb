daf0826311f49b2f34039cccee014dacf01a77b4
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-22 17:37:46 -0500

    commit tree 64bee5e556f64d4c6612eda97fca62f228588007

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3]
+  end
+
+  def valid?
+    @sides.all?(&:positive?) && @sides.min(2).sum > @sides.max
+  end
+
+  def kind
+    if !valid?
+      raise TriangleError
+    else
+      if @sides.uniq.size <= 1
+        :equilateral
+      elsif @sides.uniq.size <= 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
